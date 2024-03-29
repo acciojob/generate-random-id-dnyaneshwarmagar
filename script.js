f0412856480475cb8caeb8a964c2db4f0fc9f3c0
Dnyaneshwar Magar <89983890+dnyaneshwarmagar@users.noreply.github.com>
2024-03-29 12:45:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function makeid(l) {
   // write your code here
+	let charlist = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	let result = ""
+	for(let i=0; ,i < l; i++){
+		result += charlist.charCodeAt(Math.floor(Math.random()*charlist.length))
+	}
+
+	return result;
+	
 }
 
 // Do not change the code below.
